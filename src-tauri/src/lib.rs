e63e2eba6ab1dee2832c1698aa4b31252724acd7
aaron willem meinema <aaronwillem@hotmail.com>
2025-01-22 20:21:42 +0100

    reload needed for display, backend ready to display files in sidebar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,12 @@
-use tauri::{menu::{MenuBuilder, MenuItemBuilder, SubmenuBuilder}, Emitter};
-use tauri_plugin_dialog::{FileDialogBuilder};
+use std::{f32::consts::E, fs::{self, ReadDir}, path::Path};
+
+use serde::Serialize;
+use tauri::{
+    menu::{MenuBuilder, MenuItemBuilder, SubmenuBuilder},
+    Emitter
+};
+use tauri_plugin_dialog::{DialogExt, FilePath};
+use walkdir::WalkDir;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -7,21 +14,51 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[derive(Clone, Serialize)]
+#[serde(rename_all="camelCase")]
+enum FileType {
+    Main,
+    File,
+    Folder
+}
+
+#[derive(Clone, Serialize)]
+#[serde(rename_all="camelCase")]
+struct ProgramFile {
+    file_type: FileType,
+    name: String
+}
+
+fn get_all_files(directory: FilePath) -> Vec<ProgramFile>{
+    let mut paths: Vec<ProgramFile> = Vec::new();
+    for entry in WalkDir::new(directory.to_string()).into_iter().filter_map(Result::ok) {
+        let the_file_type: FileType;
+        if entry.file_type().is_file() {
+            the_file_type = FileType::File;
+        } else {
+            the_file_type = FileType::Folder;
+        }
+        paths.push(ProgramFile{
+            file_type: the_file_type,
+            name: entry 
+                .path()
+                .display()
+                .to_string()
+        }); 
+    }
+    paths.push(ProgramFile{
+        file_type: FileType::Main,
+        name: directory.to_string(),
+    });
+    paths
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
-            let handle = app.handle();
-
-            FileDialogBuilder::new()
-                .set_title("Choose a file") 
-                .pick_file(|file_path| 
-                    match file_path { 
-                        Some(path) => println!("Selected file path: {:?}", path),
-                        None => println!("No file selected"),
-                    });
-            
             // my custom settings menu item
             let open = MenuItemBuilder::new("open")
                 .id("open")
@@ -52,11 +89,20 @@ pub fn run() {
             // listen for menu item click events
             app.on_menu_event(move |app, event| {
                 if event.id() == open.id() {
-                    // emit a window event to the frontend 
-                    let _event = app.emit("custom-event", "/settings");
+                    // emit a window event to the frontend
+                    let app_clone = app.clone();
+                    let _event = app.emit(
+                        "open",
+                        app.dialog().file().pick_folder(move |folder| {
+                                    if folder.is_some() {
+                                        app_clone.emit_to("okee", "open_files", 
+                                        get_all_files(folder.unwrap())).unwrap();
+                                    }
+                                }
+                            )
+                    );
                 }
             });
-
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
+
 <template class="container">
   <main class="container">
+    <div class="files">
+      <li v-for="file in files" :key="files">
+        {{ file.name }}
+      </li>
+    </div>
     <textarea class="text"></textarea>
   </main>
 </template>
+<script lang="ts">
+  import { listen } from '@tauri-apps/api/event'
+import { Ref, ref } from 'vue';
+  enum FileType {
+    main,
+    file,
+    folder
+  }
+
+
+  type FilePath = {
+    fileType: FileType,
+    name: string
+  }
+  let files: Ref<FilePath[]> = ref([]);
+
+  listen<[FilePath]>('open_files', (event) => {
+    files = ref(event.payload);
+  });
+
+</script>
